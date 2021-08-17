2434094c0f2a77ca2766b234194ac5172d4589be
Erchegyi David <edavid@2-H-2.42heilbronn.de>
2021-08-17 19:43:12 +0200

    usleep does not fix the issue, the signal gets sent first before server could catch it

--- client.c
+++ client.c
@@ -2,35 +2,52 @@
 #include <signal.h>
 #include <stdlib.h>
 #include "minitalk.h"
+#include <stdio.h>
+
+void	handle_error(void)
+{
+	char	*tmp;
+
+	tmp = "Usage: client PID tmp\n";
+	write(STDERR_FILENO, tmp, ft_strlen(tmp));
+	exit(EXIT_FAILURE);
+}
+
+void	handlesigusr(int sig)
+{
+
+}
 
 int main(int argc, char **argv)
 {
 	char				*tmp;
-	char				*message;
 	char				cur;
 	pid_t				pid;
 	int					i;
+	struct sigaction	sa;
 
+	printf("Client pid: %d\n", (int)getpid());
 	if (argc != 3)
-	{
-		tmp = "Usage: client PID tmp\n";
-		write(STDERR_FILENO, tmp, ft_strlen(tmp));
-		exit(EXIT_FAILURE);
-	}
+		handle_error();
+	sa.__sigaction_u.__sa_handler = &handlesigusr;
+	sa.sa_flags = SA_RESTART;
+	sigaction(SIGUSR1, &sa, NULL);
 	pid = ft_atoi(argv[1]);
-	message = argv[2];
-	tmp = message;
+	tmp = argv[2];
 	while (*tmp)
 	{
 		cur = *tmp;
 		i = -1;
 		while (++i < 8)
 		{
-			if (cur & 1)
+			if (cur & 128)
 				kill(pid, SIGUSR1);
 			else
 				kill(pid, SIGUSR2);
-			cur >>= 1;
+			cur <<= 1;
+			usleep(1000);
+			pause();
+			usleep(1000);
 		}
 		tmp++;
 	}
--- server.c
+++ server.c
@@ -2,35 +2,49 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <inttypes.h>
 
-void	handlesigusr1(int sig)
+void	handlesigusr(int sig, siginfo_t *info, void *ucontext)
 {
+	static char	c;
+	static int	i;
 
+	if (sig == SIGUSR1)
+	{
+		c <<= 1;
+		c |= 1;
+	}
+	else if (sig == SIGUSR2)
+		c <<= 1;
+	i++;
+	if (i == 8)
+	{
+		write(STDOUT_FILENO, &c, 1);
+		i = 0;
+		c = 0;
+	}
+	printf("%d ", info->si_pid);
+	if (info->si_pid)
+		kill(info->si_pid, SIGUSR1);
 }
 
 int	main(void)
 {
 	pid_t				pid;
-	sigset_t			mask1;
-	sigset_t			mask2;
-	struct sigaction	sa1;
-	struct sigaction	sa2;
+	struct sigaction	sa;
 
 	pid = getpid();
 	ft_putnbr_fd((int)pid, STDOUT_FILENO);
-	sigemptyset(&mask1);
-	sigaddset(&mask1, SIGUSR2);
-	sa1.sa_handler = &handlesigusr1;
-	sa1.sa_mask = mask1;
-	sigemptyset(&mask2);
-	sigaddset(&mask2, SIGUSR1);
-	sa2.sa_handler = &handlesigusr2;
-	sa2.sa_mask = mask2;
-	sigaction(SIGUSR1, &sa1, NULL);
-	sigaction(SIGUSR2, &sa2, NULL);
+	sa.__sigaction_u.__sa_sigaction = &handlesigusr;
+	sa.sa_flags = SA_RESTART;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
 	while (1)
 	{
+		usleep(1000);
 		pause();
+		usleep(1000);
 	}
 	return (0);
 }
